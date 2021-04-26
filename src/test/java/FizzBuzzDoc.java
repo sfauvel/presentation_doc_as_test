c94bf9c5f66867b18d10f2bed43b1a7f00abd7e4
sfauvel <sfauvel@gmail.com>
2021-04-26 17:25:29 +0200

    Split in differents methods

--- src/test/java/FizzBuzzDoc.java
+++ src/test/java/FizzBuzzDoc.java
@@ -40,13 +40,43 @@ class FizzBuzzDoc {
         write("http://codingdojo.org/kata/FizzBuzz/");
         write("");
 
-        write("== Sample output");
-        write("");
-        write(IntStream.rangeClosed(1, 20)
-                .mapToObj(FizzBuzz::get)
-                .collect(Collectors.joining(" +\n")));
-        write("... etc up to 100");
+        sample_output();
 
+        return_given_value();
+
+        return_fizz_when_divisible_by_three();
+
+        return_buzz_when_divisible_by_five();
+
+        return_fizzbuzz_when_divisible_by_three_and_five();
+
+    }
+
+    private void return_fizzbuzz_when_divisible_by_three_and_five() throws IOException {
+        write("== Return fizzbuzz when divisible by three and five");
+        write("When number is divisible by three and five, returns FizzBuzz.");
+        write("");
+        callFizzBuzzWith(15);
+        callFizzBuzzWith(60);
+    }
+
+    private void return_buzz_when_divisible_by_five() throws IOException {
+        write("== Return buzz when divisible by five");
+        write("When number is divisible by five, returns Buzz.");
+        write("");
+        callFizzBuzzWith(5);
+        callFizzBuzzWith(25);
+    }
+
+    private void return_fizz_when_divisible_by_three() throws IOException {
+        write("== Return fizz when divisible by three");
+        write("When number is divisible by three, returns Fizz.");
+        write("");
+        callFizzBuzzWith(3);
+        callFizzBuzzWith(9);
+    }
+
+    private void return_given_value() throws IOException {
         write("== Return given number");
         write("When number is not divisible by five or three, returns the number.");
         write("");
@@ -54,25 +84,15 @@ class FizzBuzzDoc {
         callFizzBuzzWith(2);
         callFizzBuzzWith(4);
         callFizzBuzzWith(22);
+    }
 
-        write("== Return fizz when divisible by three");
-        write("When number is divisible by three, returns Fizz.");
+    private void sample_output() throws IOException {
+        write("== Sample output");
         write("");
-        callFizzBuzzWith(3);
-        callFizzBuzzWith(9);
-
-        write("== Return buzz when divisible by five");
-        write("When number is divisible by five, returns Buzz.");
-        write("");
-        callFizzBuzzWith(5);
-        callFizzBuzzWith(25);
-
-        write("== Return fizzbuzz when divisible by three and five");
-        write("When number is divisible by three and five, returns FizzBuzz.");
-        write("");
-        callFizzBuzzWith(15);
-        callFizzBuzzWith(60);
-
+        write(IntStream.rangeClosed(1, 20)
+                .mapToObj(FizzBuzz::get)
+                .collect(Collectors.joining(" +\n")));
+        write("... etc up to 100");
     }
 
     private void callFizzBuzzWith(int value) throws IOException {
