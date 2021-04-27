7ae0bcf6ef6b345e4e4838ba8bcdc8b1bf9502ca
sfauvel <sfauvel@gmail.com>
2021-04-27 09:15:07 +0200

    Add a chapter grouping by values and code extraction

--- src/test/java/demo/FizzBuzzDoc.java
+++ src/test/java/demo/FizzBuzzDoc.java
@@ -1,10 +1,16 @@
 package demo;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.sfvl.docformatter.AsciidocFormatter;
 import org.sfvl.doctesting.junitextension.ApprovalsExtension;
+import org.sfvl.doctesting.utils.CodeExtractor;
 import org.sfvl.doctesting.utils.DocWriter;
 
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -67,6 +73,54 @@ class FizzBuzzDoc {
     public void return_fizzbuzz_when_divisible_by_three_and_five() {
         callFizzBuzzWith(15);
         callFizzBuzzWith(60);
+    }
+
+    @Test
+    public void fizzbuzz_usage(TestInfo testInfo) {
+        // >>>
+        final String result = FizzBuzz.get(2);
+        // <<<
+        final AsciidocFormatter formatter = new AsciidocFormatter();
+        doc.write(String.format("With the following code, result is `%s`", result));
+        doc.write(formatter.sourceCode(
+                CodeExtractor.extractPartOfMethod(testInfo.getTestMethod().get())
+        ));
+    }
+
+    @Test
+    @DisplayName(value = "Values by result")
+    public void values_for_a_result() {
+        final int maxValue = 30;
+        final int minValue = 1;
+
+        final Map<String, List<Integer>> groupedValues = IntStream.rangeClosed(minValue, maxValue).boxed()
+                .collect(Collectors.groupingBy(
+                        i -> returnValueType(FizzBuzz.get(i))
+                ));
+
+        doc.write(String.format("List of values (between %d to %d) returned by FizzBuzz:", minValue, maxValue),
+                "",
+                groupedValues.entrySet().stream()
+                        .map(e -> String.format("* *%s*: %s", e.getKey(), e.getValue()))
+                        .collect(Collectors.joining("\n")));
+    }
+
+    public static String returnValueType(String fizzBuzzResult) {
+        try {
+            Integer.parseInt(fizzBuzzResult);
+            return "Number";
+        } catch (NumberFormatException nfe) {
+            return fizzBuzzResult;
+        }
+    }
+
+    public static boolean isNumeric(String strNum) {
+        try {
+            Integer.parseInt(strNum);
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
     }
 
     private void callFizzBuzzWith(int value) {
